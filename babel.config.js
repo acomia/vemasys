025e0a6f6aa47e92256b6214e6c0e65e27dddbff
gene <gene@skynet.local>
2022-06-02 11:04:44 +0800

    feat: module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = api => {
+  api.cache(true);
+
+  const modules = [
+    'api',
+    'assets',
+    'components',
+    'config',
+    'constants',
+    'hooks',
+    'fixtures',
+    'navigation',
+    'screens',
+    'stores',
+    'helpers',
+  ];
+
+  const alias = modules.reduce((aliasAcc, moduleName) => {
+    aliasAcc[`${moduleName}`] = `./src/${moduleName}`;
+    return aliasAcc;
+  });
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: '.',
+          alias,
+        },
+      ],
+    ],
+  };
 };
