cb13529db70f8ab8343b651e9ac9120eb4083b5d
Eugene <eugene@bluecentury.eu>
2022-06-02 15:32:01 +0800

    chore: remove babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,3 @@
-module.exports = api => {
-  api.cache(true);
-
-  const modules = [
-    'api',
-    'assets',
-    'components',
-    'config',
-    'constants',
-    'hooks',
-    'fixtures',
-    'navigation',
-    'screens',
-    'stores',
-    'helpers',
-  ];
-
-  const alias = modules.reduce((aliasAcc, moduleName) => {
-    aliasAcc[`${moduleName}`] = `./src/${moduleName}`;
-    return aliasAcc;
-  });
-
-  return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: '.',
-          alias,
-        },
-      ],
-    ],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
 };
