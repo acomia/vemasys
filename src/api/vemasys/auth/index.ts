715f9f0cc5b0de437c6cfb5ecadc5d669114b820
Eugene <eugene@bluecentury.eu>
2022-06-06 17:27:45 +0800

    feat: updates on auth API

--- src/api/vemasys/auth/index.ts
+++ src/api/vemasys/auth/index.ts
@@ -1,3 +1,46 @@
-export * from './login';
-export * from './logout';
-export * from './refreshToken';
+import {API} from '../../apiService';
+import {TCredentials, TUser} from '@bluecentury/api/models';
+
+const login = (userCredentials: TCredentials) => {
+  return API.post<TUser>('./login', userCredentials)
+    .then(response => {
+      if (response.data) {
+        return response.data;
+      } else {
+        throw Error('Request Failed');
+      }
+    })
+    .catch(error => {
+      console.error('Error: API Login ', error);
+    });
+};
+
+const logout = (userCredentials: TCredentials) => {
+  return API.post<TUser>('./login', userCredentials)
+    .then(response => {
+      if (response.data) {
+        return response.data;
+      } else {
+        throw Error('Request Failed');
+      }
+    })
+    .catch(error => {
+      console.error('Error: API Logout ', error);
+    });
+};
+
+const resetToken = (userCredentials: TCredentials) => {
+  return API.post<TUser>('./login', userCredentials)
+    .then(response => {
+      if (response.data) {
+        return response.data;
+      } else {
+        throw Error('Request Failed');
+      }
+    })
+    .catch(error => {
+      console.error('Error: API Reset Token ', error);
+    });
+};
+
+export {login, logout, resetToken};
