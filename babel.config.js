a398a08f46c7a48a4d64b46ec27535ac0938f233
Eugene <eugene@bluecentury.eu>
2022-06-06 17:47:37 +0800

    feat: setup env variables module

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['module:react-native-dotenv', {
-      'moduleName': 'react-native-dotenv',
+      'moduleName': '@bluecentury/env',
     }],
     ['react-native-reanimated/plugin'],
   ],
