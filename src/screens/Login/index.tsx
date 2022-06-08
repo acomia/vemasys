daedb6a2d318200baa6f742b9d13531ae7a829e7
Eugene <eugene@bluecentury.eu>
2022-06-09 02:08:58 +0800

    feat: updated screen implementations

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,18 +1,20 @@
 import React, {useEffect, useState} from 'react'
-import {ActivityIndicator, StyleSheet, TouchableOpacity} from 'react-native'
-import {SafeAreaView} from 'react-native-safe-area-context'
+import {ActivityIndicator} from 'react-native'
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
 import {
-  View,
+  Box,
+  VStack,
   Text,
   Image,
   FormControl,
   Icon,
   Input,
   Button,
-  WarningOutlineIcon
+  WarningOutlineIcon,
+  Center
 } from 'native-base'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import {CommonActions} from '@react-navigation/native'
 import {ms} from 'react-native-size-matters'
 import {Colors} from '@bluecentury/styles'
 import {useAuth} from '@bluecentury/stores'
@@ -22,11 +24,15 @@ import {_t} from '@bluecentury/constants'
 
 type Props = NativeStackScreenProps<RootStackParamList>
 
-export default function Login({navigation}: Props) {
+function Login({navigation}: Props) {
   const {authenticate, isAuthenticatingUser, token} = useAuth()
   useEffect(() => {
     if (token) {
-      navigation.navigate('SelectEntity')
+      navigation.dispatch(
+        CommonActions.reset({
+          routes: [{name: 'Main'}]
+        })
+      )
     }
   }, [navigation, token])
 
@@ -52,106 +58,95 @@ export default function Login({navigation}: Props) {
   }
 
   return (
-    <SafeAreaView style={loginStyles.container}>
-      <View style={loginStyles.content}>
-        <TouchableOpacity activeOpacity={1}>
-          <Image
-            alt="Company Logo"
-            source={Images.logo}
-            resizeMode="contain"
-            style={loginStyles.logo}
-          />
-        </TouchableOpacity>
-        <Text style={loginStyles.loginLabel}>Login to your Account</Text>
-        <FormControl isInvalid={isUsernameEmpty} mb="5" mt="5">
-          <Input
-            value={user.username}
-            onChangeText={text => {
-              setUser({...user, username: text}), setIsUsernameEmpty(false)
-            }}
-            placeholder="Username"
-            InputLeftElement={
-              <Icon
-                as={<Ionicons name="person-outline" />}
-                size={ms(20)}
-                color={Colors.azure}
-                marginLeft={ms(10)}
-              />
-            }
-            _disabled={{bgColor: '#ADADAD'}}
-            fontSize={ms(15)}
-            size="lg"
-          />
-          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-            {_t('usernameRequired')}
-          </FormControl.ErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={isPasswordEmpty} mb="4">
-          <Input
-            value={user.password}
-            onChangeText={text => {
-              setUser({...user, password: text}), setIsPasswordEmpty(false)
-            }}
-            type={isShowPassword ? 'text' : 'password'}
-            placeholder="Password"
-            InputLeftElement={
-              <Icon
-                as={<Ionicons name="lock-open-outline" />}
-                size={ms(20)}
-                color="#23475C"
-                marginLeft={ms(10)}
-              />
-            }
-            InputRightElement={
-              <Icon
-                as={
-                  <Ionicons
-                    name={isShowPassword ? 'eye-outline' : 'eye-off-outline'}
-                  />
-                }
-                size={ms(20)}
-                color={Colors.azure}
-                marginRight={ms(10)}
-                onPress={() => setIsShowPassword(!isShowPassword)}
-              />
-            }
-            fontSize={ms(15)}
-            color={Colors.disabled}
-            size="lg"
-          />
-          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-            {user.username === '' && user.password === ''
-              ? _t('usernamePasswordRequired')
-              : _t('passwordRequired')}
-          </FormControl.ErrorMessage>
-        </FormControl>
+    <Box flex="1" safeArea>
+      <VStack space="10" flex="1" p="5" justifyContent="center">
+        <Center>
+          <Image alt="Company Logo" source={Images.logo} resizeMode="contain" />
+        </Center>
+        <VStack space="5">
+          <Text bold fontSize="2xl" color={Colors.azure}>
+            Login to your Account
+          </Text>
+          <FormControl isInvalid={isUsernameEmpty}>
+            <Input
+              value={user.username}
+              onChangeText={text => {
+                setUser({...user, username: text})
+                setIsUsernameEmpty(false)
+              }}
+              placeholder="Username"
+              InputLeftElement={
+                <Icon
+                  as={<Ionicons name="person-outline" />}
+                  size={ms(20)}
+                  color={Colors.azure}
+                  marginLeft={ms(10)}
+                />
+              }
+              _disabled={{bgColor: '#ADADAD'}}
+              fontSize={ms(15)}
+              size="lg"
+            />
+            <FormControl.ErrorMessage
+              leftIcon={<WarningOutlineIcon size="xs" />}>
+              {_t('usernameRequired')}
+            </FormControl.ErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={isPasswordEmpty}>
+            <Input
+              value={user.password}
+              onChangeText={text => {
+                setUser({...user, password: text})
+                setIsPasswordEmpty(false)
+              }}
+              type={isShowPassword ? 'text' : 'password'}
+              placeholder="Password"
+              InputLeftElement={
+                <Icon
+                  as={<Ionicons name="lock-open-outline" />}
+                  size={ms(20)}
+                  color="#23475C"
+                  marginLeft={ms(10)}
+                />
+              }
+              InputRightElement={
+                <Icon
+                  as={
+                    <Ionicons
+                      name={isShowPassword ? 'eye-outline' : 'eye-off-outline'}
+                    />
+                  }
+                  size={ms(20)}
+                  color={Colors.azure}
+                  marginRight={ms(10)}
+                  onPress={() => setIsShowPassword(!isShowPassword)}
+                />
+              }
+              fontSize={ms(15)}
+              color={Colors.disabled}
+              size="lg"
+            />
+            <FormControl.ErrorMessage
+              leftIcon={<WarningOutlineIcon size="xs" />}>
+              {user.username === '' && user.password === ''
+                ? _t('usernamePasswordRequired')
+                : _t('passwordRequired')}
+            </FormControl.ErrorMessage>
+          </FormControl>
+        </VStack>
         <Button
-          marginTop="1/6"
           colorScheme="azure"
           onPress={onUserLogin}
           isLoading={isAuthenticatingUser}
-          _loading={<ActivityIndicator size="small" />}>
-          LOGIN
+          _loading={<ActivityIndicator size="small" />}
+          _text={{
+            textTransform: 'uppercase'
+          }}>
+          {_t('login')}
         </Button>
-      </View>
-    </SafeAreaView>
+      </VStack>
+    </Box>
   )
 }
 
-const loginStyles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  content: {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center'
-  },
-  logo: {
-    width: 312,
-    height: 77,
-    alignSelf: 'center',
-    marginBottom: '20%'
-  },
-  loginLabel: {fontSize: 22, fontWeight: '600', color: '#23475C'}
-})
+export default Login
--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,8 +1,10 @@
-import React, {useEffect} from 'react'
-import {Text, View, Button} from 'native-base'
-import {useAuth} from '@bluecentury/stores'
+import React, {useEffect, useRef} from 'react'
+import {ActivityIndicator} from 'react-native'
+import {Box, Center, Image} from 'native-base'
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
 import {CommonActions} from '@react-navigation/native'
+import {Images} from '@bluecentury/assets'
+import {useAuth} from '@bluecentury/stores'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Splash'>
 
@@ -11,19 +13,38 @@ export default function Splash({navigation}: Props) {
   // uncomment below to logout user
   useEffect(() => {
     logout()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  let timeout = useRef<any>()
   useEffect(() => {
     if (token) {
-      navigation.dispatch(CommonActions.navigate({name: 'Main'}))
+      timeout.current = setTimeout(() => {
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'Main'}]
+          })
+        )
+      }, 1500)
     } else {
-      navigation.dispatch(CommonActions.navigate({name: 'Login'}))
+      timeout.current = setTimeout(() => {
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'Login'}]
+          })
+        )
+      }, 1500)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => clearTimeout(timeout.current)
   }, [])
+
   return (
-    <View>
-      <Text>test</Text>
-    </View>
+    <Box flex="1" justifyContent="center" safeArea>
+      <Center>
+        <Image alt="Company Logo" source={Images.logo} />
+        <ActivityIndicator size="small" />
+      </Center>
+    </Box>
   )
 }
