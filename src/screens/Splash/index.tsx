2aa327bdcd57c6aa7fe70668ffcf9c2dbf3da481
Eugene <eugene@bluecentury.eu>
2022-06-09 01:11:01 +0800

    chore: remove semicolon

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,29 +1,29 @@
-import React, {useEffect} from 'react';
-import {Text, View, Button} from 'native-base';
-import {useAuth} from '@bluecentury/stores';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {CommonActions} from '@react-navigation/native';
+import React, {useEffect} from 'react'
+import {Text, View, Button} from 'native-base'
+import {useAuth} from '@bluecentury/stores'
+import {NativeStackScreenProps} from '@react-navigation/native-stack'
+import {CommonActions} from '@react-navigation/native'
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Splash'>;
+type Props = NativeStackScreenProps<RootStackParamList, 'Splash'>
 
 export default function Splash({navigation}: Props) {
-  const {token, logout} = useAuth();
+  const {token, logout} = useAuth()
   // uncomment below to logout user
   useEffect(() => {
-    logout();
+    logout()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
   useEffect(() => {
     if (token) {
-      navigation.dispatch(CommonActions.navigate({name: 'Main'}));
+      navigation.dispatch(CommonActions.navigate({name: 'Main'}))
     } else {
-      navigation.dispatch(CommonActions.navigate({name: 'Login'}));
+      navigation.dispatch(CommonActions.navigate({name: 'Login'}))
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
   return (
     <View>
       <Text>test</Text>
     </View>
-  );
+  )
 }
