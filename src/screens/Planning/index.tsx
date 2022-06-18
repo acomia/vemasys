7362e272d06ade2eee265717a8ab53c0097bdd4c
Eugene <eugene@bluecentury.eu>
2022-06-18 19:41:13 +0800

    feat: use constants declared

---
+++ src/screens/Planning/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Box, Text} from 'native-base'
+
+export default function Planning() {
+  return (
+    <Box height={500} safeArea>
+      <Text>Planning</Text>
+    </Box>
+  )
+}
