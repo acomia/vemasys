281c2ab5fc393f5a0043a7b090cca1d60a80212b
Eugene <eugene@bluecentury.eu>
2022-07-09 10:07:11 +0800

    fix: remove addtl. warning

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import {AppRegistry, LogBox} from 'react-native'
 import App from './src'
 import {name as appName} from './app.json'
 
-LogBox.ignoreLogs(['`Image`'])
+LogBox.ignoreLogs([
+  '`Image`',
+  'ViewPropTypes will be removed from React Native.'
+])
 
 AppRegistry.registerComponent(appName, () => App)
