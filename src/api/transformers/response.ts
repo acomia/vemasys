1559f060a7f78dab835f0ef362745e77641c5321
Eugene <eugene@bluecentury.eu>
2022-07-13 22:52:33 +0800

    feat: refreshtoken

--- src/api/transformers/response.ts
+++ src/api/transformers/response.ts
@@ -1,10 +1,12 @@
 import {useAuth} from '@bluecentury/stores'
 import {ResponseTransform} from 'apisauce'
 import camelcaseKeys from 'camelcase-keys'
+import {API} from '../apiService'
 
 export const dataToCamelCase: ResponseTransform = async response => {
   if (response.originalError?.config) {
     try {
+      API.deleteHeader('Jwt-Auth')
       const res = await useAuth.getState().resetToken()
       if (res) {
         return Promise.resolve(response.originalError.config)
