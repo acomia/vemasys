bb4bcd63a5e66ea2ecc41cf5f6ed046a090497d5
Eugene <eugene@bluecentury.eu>
2022-07-28 23:16:15 +0800

    fix: auth

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {API_URL} from '@bluecentury/env'
 import {onFailedResponse, onSuccessfulResponse} from './interceptors'
+import {useAuth} from '@bluecentury/stores'
 
 export const API = axios.create({
   baseURL: API_URL,
@@ -13,3 +14,14 @@ export const API = axios.create({
 // API.interceptors.request.use(paramsToSnakeCase)
 // API.addResponseTransform(dataToCamelCase)
 API.interceptors.response.use(onSuccessfulResponse, onFailedResponse)
+API.interceptors.request.use(async req => {
+  const token = useAuth.getState().token
+  if (!token) {
+    return req
+  }
+  req.headers = {
+    ...req.headers,
+    'Jwt-Auth': `Bearer ${token}`
+  }
+  return req
+})
--- src/api/interceptors/response/onFailedResponse.ts
+++ src/api/interceptors/response/onFailedResponse.ts
@@ -1,15 +1,37 @@
+import axios from 'axios'
+import {useAuth} from '@bluecentury/stores'
 import {API} from '../../apiService'
+import {API_URL} from '@bluecentury/env'
 
 const UNAUTHENTICATED = 401
 
 export const onFailedResponse = async (error: any) => {
   // DO anything here
-
+  console.log('Error data: ', error?.response.data)
+  console.log('Error status: ', error?.response.status)
+  console.log('Error request url: ', error?.config.headers['Jwt-Auth'])
   const failedRequest = error?.config
 
   if (error?.response?.status === UNAUTHENTICATED) {
-    // REFRESH TOKEN HERE
-    return API(failed)
+    console.log('test')
+    try {
+      const refreshToken = useAuth.getState().refreshToken
+      const res = await axios.post(
+        `${API_URL}/token/refresh?refresh_token=${refreshToken}`
+      )
+      if (res) {
+        console.log('res ', res)
+        failedRequest.defaults.headers = {
+          ...failedRequest.defaults.headers,
+          'Jwt-Auth': `Bearer ${res.data.token}`
+        }
+        return API(failedRequest)
+      }
+    } catch (err) {
+      console.log('Error: Failed to Refresh Token ', err)
+      useAuth.getState().logout()
+      return Promise.reject(err)
+    }
   }
   return Promise.reject(error)
 }
