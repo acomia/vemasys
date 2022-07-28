e3e802e32e7cca7787a14205b9c5ec8ecc95f43f
Eugene <eugene@bluecentury.eu>
2022-07-28 23:22:40 +0800

    feat: transform response

--- src/api/interceptors/response/onSuccessfulResponse.ts
+++ src/api/interceptors/response/onSuccessfulResponse.ts
@@ -1,3 +1,8 @@
+import camelcaseKeys from 'camelcase-keys'
+
 export const onSuccessfulResponse = async (response: any) => {
+  if (response.data) {
+    response.data = camelcaseKeys(response.data, {deep: true})
+  }
   return response
 }
