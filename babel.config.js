7d69461f5be8be714ca88876cecb0530da7603d7
Eugene <eugene@bluecentury.eu>
2022-07-29 02:40:51 +0800

    feat: add module-resolver

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['module-resolver', {
+      root: ['./src'],
+      alias: {
+        '@bluecentury/utils': ['./src/utils']
+      }
+    }],
     ['module:react-native-dotenv', {
       'moduleName': '@bluecentury/env',
     }],
