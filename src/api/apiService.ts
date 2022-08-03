790bb37f3ff58799aa08007ffbe8aab6d4eaf645
Eugene <eugene@bluecentury.eu>
2022-08-03 18:21:51 +0800

    fix: add 'X-active-entity-user-id' in request headers

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {API_URL} from '@bluecentury/env'
 import {onFailedResponse, onSuccessfulResponse} from './interceptors'
-import {useAuth} from '@bluecentury/stores'
+import {useAuth, useEntity} from '@bluecentury/stores'
 
 export const API = axios.create({
   baseURL: API_URL,
@@ -16,12 +16,24 @@ export const API = axios.create({
 API.interceptors.response.use(onSuccessfulResponse, onFailedResponse)
 API.interceptors.request.use(async req => {
   const token = useAuth.getState().token
+  const entityId = useEntity.getState().entityId
+
   if (!token) {
     return req
   }
   req.headers = {
     ...req.headers,
     'Jwt-Auth': `Bearer ${token}`
+  }
+
+  if (typeof entityId !== 'undefined') {
+    req.headers = {
+      ...req.headers,
+      'X-active-entity-user-id': `${entityId}`
+    }
+  }
+  if (__DEV__) {
+    console.log('Request Url: ', req.url)
   }
   return req
 })
