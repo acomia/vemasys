6cb3934b64e6dede44b471332ff3fc80b26d1d1f
Eugene <eugene@bluecentury.eu>
2022-08-03 18:23:10 +0800

    fix: added some logging

--- src/api/interceptors/response/onFailedResponse.ts
+++ src/api/interceptors/response/onFailedResponse.ts
@@ -6,21 +6,24 @@ import {API_URL} from '@bluecentury/env'
 const UNAUTHENTICATED = 401
 
 export const onFailedResponse = async (error: any) => {
+  const failedRequest = error?.config
+  let failedRequestHeaders = failedRequest.headers || undefined
   // DO anything here
   console.log('Error data: ', error?.response.data)
   console.log('Error status: ', error?.response.status)
   console.log('Error request url: ', error?.config.url)
-  const failedRequest = error?.config
+  console.log(
+    'Error request headers: ',
+    failedRequestHeaders['X-active-entity-user-id']
+  )
 
   if (error?.response?.status === UNAUTHENTICATED) {
-    console.log('test')
     try {
       const refreshToken = useAuth.getState().refreshToken
       const res = await axios.post(
         `${API_URL}/token/refresh?refresh_token=${refreshToken}`
       )
       if (res) {
-        console.log('res ', res)
         failedRequest.defaults.headers = {
           ...failedRequest.defaults.headers,
           'Jwt-Auth': `Bearer ${res.data.token}`
