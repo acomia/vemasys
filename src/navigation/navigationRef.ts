9e467a92fda6745134a5f352f9148950e5fe42a3
Eugene <eugene@bluecentury.eu>
2022-08-05 09:23:49 +0800

    feat: create navigationRef

--- src/navigation/navigationRef.ts
+++ src/navigation/navigationRef.ts
@@ -2,7 +2,7 @@ import {createNavigationContainerRef} from '@react-navigation/native'
 
 export const navigationRef = createNavigationContainerRef<RootStackParamList>()
 
-export function navigate(name: any, params: any) {
+export function navigate(name: any, params?: any) {
   if (navigationRef.isReady()) {
     navigationRef.navigate(name, params)
   }
