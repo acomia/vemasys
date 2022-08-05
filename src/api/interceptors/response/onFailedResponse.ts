1ded190e2b1ed16adda66d01e8fdd4d954c404d6
Eugene <eugene@bluecentury.eu>
2022-08-05 11:22:44 +0800

    feat: dont refresh token on failed auth

--- src/api/interceptors/response/onFailedResponse.ts
+++ src/api/interceptors/response/onFailedResponse.ts
@@ -7,21 +7,24 @@ const UNAUTHENTICATED = 401
 
 export const onFailedResponse = async (error: any) => {
   const failedRequest = error?.config
+  const errorUrl = failedRequest?.url
   let failedRequestHeaders = failedRequest.headers || undefined
   // DO anything here
   console.log('Error data: ', error?.response.data)
   console.log('Error status: ', error?.response.status)
-  console.log('Error request url: ', error?.config.url)
+  console.log('Error request url: ', errorUrl)
   console.log(
     'Error request headers: ',
     failedRequestHeaders['X-active-entity-user-id']
   )
 
-  if (error?.response?.status === UNAUTHENTICATED) {
+  const isLogin = errorUrl === 'login_check'
+
+  if (error?.response?.status === UNAUTHENTICATED && !isLogin) {
     try {
       const refreshToken = useAuth.getState().refreshToken
       const res = await axios.post(
-        `${API_URL}/token/refresh?refresh_token=${refreshToken}`
+        `${API_URL}token/refresh?refresh_token=${refreshToken}`
       )
       if (res) {
         failedRequest.defaults.headers = {
