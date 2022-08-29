79529680861e8aa4bbd132513f3fdf6efb58ba96
Eugene <eugene@bluecentury.eu>
2022-08-29 15:14:39 +0800

    feat: add select environment if not yet selected

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import {ActivityIndicator} from 'react-native'
 import {Box, Center, Image} from 'native-base'
 import {Images} from '@bluecentury/assets'
-import {useAuth, useEntity} from '@bluecentury/stores'
+import {useAuth, useEntity, useSettings} from '@bluecentury/stores'
 // import {} from '@bluecentury/utils'
 import {
   CommonActions,
@@ -14,24 +14,35 @@ export default function Splash() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>()
   const {hasAuthHydrated, token} = useAuth(state => state)
   const {hasEntityHydrated, entityId} = useEntity(state => state)
+  const {hasSettingsRehydrated, apiUrl} = useSettings(state => state)
 
   useEffect(() => {
-    if (hasAuthHydrated && hasEntityHydrated) {
-      if (token && entityId) {
-        navigation.dispatch(
-          CommonActions.reset({
-            index: 0,
-            routes: [{name: 'Main'}]
-          })
-        )
-      } else if (token) {
-        navigation.navigate('SelectEntity')
+    if (hasAuthHydrated && hasEntityHydrated && hasSettingsRehydrated) {
+      if (typeof apiUrl === 'undefined') {
+        navigation.navigate('SelectEnvironment')
       } else {
-        navigation.navigate('Login')
+        if (token && entityId) {
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [{name: 'Main'}]
+            })
+          )
+        } else if (token) {
+          navigation.navigate('SelectEntity')
+        } else {
+          navigation.navigate('Login')
+        }
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasAuthHydrated, hasEntityHydrated, token, entityId])
+  }, [
+    hasAuthHydrated,
+    hasEntityHydrated,
+    hasSettingsRehydrated,
+    token,
+    entityId
+  ])
 
   return (
     <Box flex="1" justifyContent="center" safeArea>
