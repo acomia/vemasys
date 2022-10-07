64eff9c415aad5cdbc9c5d552884635b6ee67230
Eugene <eugene@bluecentury.eu>
2022-10-07 18:04:44 +0800

    chore: optional charter in param list

--- src/types/nav.types.d.ts
+++ src/types/nav.types.d.ts
@@ -30,7 +30,7 @@ type RootStackParamList = {
   AddCrewMember: undefined
   InformationPegelDetails: {pegelId: number}
   ImgViewer: {url: string; title: string}
-  CharterAcceptSign: {charter: {} | undefined}
+  CharterAcceptSign: {charter?: any}
   TrackingServiceDialog: undefined
 }
 
