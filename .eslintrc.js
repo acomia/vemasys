a08623fe37ce64a1571a8e05b426eb30c965575c
Eugene <eug5ne@gmail.com>
2022-12-19 22:44:15 +0800

    feat: prettier eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
