6d8cdeae68823416707933167f1631f4f86a3313
Stas Motorny <slanislav@bluecentury.eu>
2023-01-20 13:50:06 +0200

    fix: notifications time is fixed, NotificationItem extracted in separate component.

--- src/screens/Notification/index.tsx
+++ src/screens/Notification/index.tsx
@@ -1,25 +1,13 @@
 import React, {useEffect} from 'react'
-import {View, SectionList, Dimensions, RefreshControl} from 'react-native'
-import {
-  Box,
-  Image,
-  Divider,
-  Flex,
-  HStack,
-  Text,
-  VStack,
-  Center,
-} from 'native-base'
+import {SectionList, RefreshControl} from 'react-native'
+import {Box, Divider, Text, VStack} from 'native-base'
 import {ms} from 'react-native-size-matters'
 import moment from 'moment'
-import Icon from 'react-native-vector-icons/FontAwesome5'
-import {Icons, Animated} from '@bluecentury/assets'
 import {useNotif, useEntity} from '@bluecentury/stores'
 import {FleetHeader, LoadingAnimated} from '@bluecentury/components'
 import {ENTITY_TYPE_EXPLOITATION_GROUP} from '@bluecentury/constants'
 import {useTranslation} from 'react-i18next'
-
-const screenWidth = Dimensions.get('screen').width
+import {NotificationItem} from '@bluecentury/components/NotificationItem'
 
 export default function Notification() {
   const {t} = useTranslation()
@@ -31,101 +19,23 @@ export default function Notification() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [vesselId])
 
-  let groupByDate = Object.values(
+  const groupByDate = Object.values(
     notifications.reduce((acc: any, item) => {
       const date = item.createdAt.split('T')[0]
-      if (!acc[date])
+      if (!acc[date]) {
         acc[date] = {
           date: date,
           data: [],
         }
+      }
       acc[date].data.push(item)
       return acc
     }, {})
   )
 
-  const sanitizedIcon = (icon: string) => {
-    return icon.replace('fas fa-', '')
-  }
-
-  const sanitizedHTML = (labelHTML: string) => {
-    let subject =
-      labelHTML.match(/<a\/?[^>](.*?)<\/a>/g) == null
-        ? ''
-        : labelHTML.match(/<a\/?[^>](.*?)<\/a>/g).map(function (str) {
-            return str.replace(/<\/?[^>]+(>|$)/g, '')
-          })
-
-    let notification = labelHTML.replace(/<\/?[^>]+(>|$)/g, '')
-    notification = notification.replace(subject, ':::')
-    notification = notification.split(':::')
-
-    return (
-      <Text>
-        {notification[0] ? notification[0] : ''}
-        <Text bold color="#29B7EF">
-          {subject}
-        </Text>
-        {notification[1] ? notification[1] : ''}
-      </Text>
-    )
-  }
-
-  const Item = ({item}: any) => {
-    const date = item.createdAt.split('T')[0]
-    return (
-      <HStack
-        alignItems="center"
-        backgroundColor="#fff"
-        px={ms(16)}
-        py={ms(10)}
-        my={ms(8)}
-        borderWidth={ms(1)}
-        borderRadius={ms(5)}
-        borderColor={!item.read ? '#BEE3F8' : '#E0E0E0'}
-        bg="#fff"
-        shadow={'1'}
-      >
-        {/* <Image
-        alt="notif-img"
-        source={Icons.completed}
-        width={ms(32)}
-        height={ms(32)}
-        resizeMode="contain"
-      /> */}
-        <Icon
-          name={sanitizedIcon(item.icon)}
-          color="#23475C"
-          size={24}
-          style={{marginHorizontal: 5}}
-        />
-        <VStack pl="10px">
-          <Text maxWidth={screenWidth - 100}>{sanitizedHTML(item.label)}</Text>
-          <Text fontSize={ms(12)} color="#ADADAD">
-            {moment(new Date()).isSame(moment(new Date(date)))
-              ? `${t('today')} | ${moment(date).format('HH:mm')}`
-              : moment(date).format('MMM DD, YYYY HH:mm')}
-          </Text>
-        </VStack>
-        {!item.read && (
-          <Box
-            position={'absolute'}
-            backgroundColor="#00A3FF"
-            top="0"
-            bottom="0"
-            left="0"
-            width="8px"
-            borderTopLeftRadius={ms(5)}
-            borderBottomLeftRadius={ms(5)}
-          />
-        )}
-      </HStack>
-    )
-  }
-
   const HeaderSection = ({title}: any) => (
     <VStack backgroundColor="#fff">
-      <Text fontSize={ms(15)} fontWeight="600" color="#23475C" my="10px">
+      <Text color="#23475C" fontSize={ms(15)} fontWeight="600" my="10px">
         {moment(new Date()).isSame(moment(new Date(title)))
           ? t('today')
           : moment(title).format('MMM DD, YYYY')}
@@ -162,26 +72,26 @@ export default function Notification() {
         />
       )}
       <Box
-        flex="1"
         backgroundColor={'#fff'}
         borderTopLeftRadius={ms(15)}
         borderTopRightRadius={ms(15)}
+        flex="1"
         p={ms(14)}
       >
         <SectionList
+          stickySectionHeadersEnabled
           refreshControl={
             <RefreshControl
-              onRefresh={onPullToReload}
               refreshing={isLoadingNotification}
+              onRefresh={onPullToReload}
             />
           }
-          sections={groupByDate}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({item}) => <Item item={item} />}
           renderSectionHeader={({section: {date}}: any) => (
             <HeaderSection title={date} />
           )}
-          stickySectionHeadersEnabled
+          keyExtractor={(item, index) => item + index}
+          renderItem={({item}) => <NotificationItem item={item} />}
+          sections={groupByDate}
           showsVerticalScrollIndicator={false}
         />
       </Box>
