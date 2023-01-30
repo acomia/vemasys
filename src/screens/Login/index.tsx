e51da3790263c07a2b658c632745467d8191c871
Stas Motorny <slanislav@bluecentury.eu>
2023-01-30 14:28:32 +0200

    feat: Checkbox and its functionality were commented, but wasn't removed as this is temporary change.

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -12,8 +12,6 @@ import {
   WarningOutlineIcon,
   Center,
   KeyboardAvoidingView,
-  HStack,
-  Checkbox,
 } from 'native-base'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import {ms} from 'react-native-size-matters'
@@ -22,8 +20,7 @@ import * as Keychain from 'react-native-keychain'
 import {Colors} from '@bluecentury/styles'
 import {Credentials} from '@bluecentury/models'
 import {Images} from '@bluecentury/assets'
-import {_t} from '@bluecentury/constants'
-import {useAuth, useSettings} from '@bluecentury/stores'
+import {useAuth} from '@bluecentury/stores'
 import {VersionBuildLabel} from '@bluecentury/components/version-build-label'
 import {useTranslation} from 'react-i18next'
 
@@ -31,7 +28,7 @@ export default function Login() {
   const {t} = useTranslation()
   const insets = useSafeAreaInsets()
   const {isAuthenticatingUser, authenticate, hasAuthenticationError} = useAuth()
-  const {isRemainLoggedIn, setIsRemainLoggedIn} = useSettings()
+  // const {isRemainLoggedIn, setIsRemainLoggedIn} = useSettings()
   const [user, setUser] = useState<Credentials>({username: '', password: ''})
   const [isShowPassword, setIsShowPassword] = useState(false)
   const [isUsernameEmpty, setIsUsernameEmpty] = useState(false)
@@ -76,7 +73,7 @@ export default function Login() {
   }, [])
 
   return (
-    <Box flex="1" safeArea>
+    <Box safeArea flex="1">
       <KeyboardAvoidingView
         h={{
           base: '100%',
@@ -84,43 +81,43 @@ export default function Login() {
         }}
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
-        <VStack space="10" flex="1" p="5" justifyContent="center">
+        <VStack flex="1" justifyContent="center" p="5" space="10">
           <Center>
             <Image
               alt="Company Logo"
-              source={Images.logo}
               resizeMode="contain"
+              source={Images.logo}
             />
           </Center>
           <VStack space="5">
-            <Text bold fontSize="2xl" color={Colors.azure}>
+            <Text bold color={Colors.azure} fontSize="2xl">
               {t('loginHeader')}
             </Text>
             <FormControl isInvalid={isUsernameEmpty || hasAuthenticationError}>
               <Input
                 ref={userNameRef}
+                InputLeftElement={
+                  <Icon
+                    as={<Ionicons name="person-outline" />}
+                    color={Colors.azure}
+                    marginLeft={ms(10)}
+                    size={ms(20)}
+                  />
+                }
+                _disabled={{bgColor: '#ADADAD'}}
+                autoCapitalize="none"
+                autoCorrect={false}
                 bg={Colors.white}
+                fontSize={ms(15)}
+                keyboardType="email-address"
+                placeholder="Username"
+                returnKeyType="next"
+                size="lg"
                 value={user.username}
                 onChangeText={text => {
                   setUser({...user, username: text})
                   setIsUsernameEmpty(false)
                 }}
-                placeholder="Username"
-                InputLeftElement={
-                  <Icon
-                    as={<Ionicons name="person-outline" />}
-                    size={ms(20)}
-                    color={Colors.azure}
-                    marginLeft={ms(10)}
-                  />
-                }
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
-                _disabled={{bgColor: '#ADADAD'}}
-                fontSize={ms(15)}
-                size="lg"
-                returnKeyType="next"
                 onSubmitEditing={() => passwordRef.current.focus()}
               />
               <FormControl.ErrorMessage
@@ -132,21 +129,13 @@ export default function Login() {
             </FormControl>
             <FormControl isInvalid={isPasswordEmpty || hasAuthenticationError}>
               <Input
-                bg={Colors.white}
                 ref={passwordRef}
-                value={user.password}
-                onChangeText={text => {
-                  setUser({...user, password: text})
-                  setIsPasswordEmpty(false)
-                }}
-                type={isShowPassword ? 'text' : 'password'}
-                placeholder="Password"
                 InputLeftElement={
                   <Icon
                     as={<Ionicons name="lock-open-outline" />}
-                    size={ms(20)}
                     color="#23475C"
                     marginLeft={ms(10)}
+                    size={ms(20)}
                   />
                 }
                 InputRightElement={
@@ -158,15 +147,23 @@ export default function Login() {
                         }
                       />
                     }
-                    size={ms(20)}
                     color={Colors.azure}
                     marginRight={ms(10)}
+                    size={ms(20)}
                     onPress={() => setIsShowPassword(!isShowPassword)}
                   />
                 }
-                fontSize={ms(15)}
+                bg={Colors.white}
                 color={Colors.disabled}
+                fontSize={ms(15)}
+                placeholder="Password"
                 size="lg"
+                type={isShowPassword ? 'text' : 'password'}
+                value={user.password}
+                onChangeText={text => {
+                  setUser({...user, password: text})
+                  setIsPasswordEmpty(false)
+                }}
                 onSubmitEditing={handleOnSubmitEditingPassword}
               />
               <FormControl.ErrorMessage
@@ -179,33 +176,33 @@ export default function Login() {
                   : passwordRequired}
               </FormControl.ErrorMessage>
             </FormControl>
-            <HStack justifyContent="flex-end">
-              <Checkbox
-                isChecked={isRemainLoggedIn}
-                onChange={v => setIsRemainLoggedIn(v)}
-                value="remain-logged-in"
-              >
-                {t('remainLogin')}
-              </Checkbox>
-            </HStack>
+            {/*<HStack justifyContent="flex-end">*/}
+            {/*  <Checkbox*/}
+            {/*    isChecked={isRemainLoggedIn}*/}
+            {/*    onChange={v => setIsRemainLoggedIn(v)}*/}
+            {/*    value="remain-logged-in"*/}
+            {/*  >*/}
+            {/*    {t('remainLogin')}*/}
+            {/*  </Checkbox>*/}
+            {/*</HStack>*/}
           </VStack>
           <Button
-            colorScheme="azure"
-            isLoadingText="Logging in"
-            isLoading={isAuthenticatingUser}
             _spinner={{
               color: Colors.white,
             }}
             _text={{
               textTransform: 'uppercase',
             }}
+            colorScheme="azure"
+            isLoading={isAuthenticatingUser}
+            isLoadingText="Logging in"
             onPress={handleOnPressLogin}
           >
             {t('login')}
           </Button>
         </VStack>
       </KeyboardAvoidingView>
-      <Box position="absolute" bottom={0} left={0} right={0} safeAreaBottom>
+      <Box safeAreaBottom bottom={0} left={0} position="absolute" right={0}>
         <Center pb={insets.bottom > 0 ? 0 : 5}>
           <VersionBuildLabel hideVersionName={true} />
         </Center>
