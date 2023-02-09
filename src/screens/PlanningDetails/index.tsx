3038478fa3661b551a608ca153d8d98fe7f40c2c
klartdominic <klart@i-cube-digitalsolutions.com>
2023-02-09 18:01:50 +0800

    fix: change the style values to standard from size matters

--- src/screens/PlanningDetails/index.tsx
+++ src/screens/PlanningDetails/index.tsx
@@ -62,12 +62,12 @@ export default function PlanningDetails({route}: Props) {
           height: 3,
           borderRadius: 3,
           width: ms(45),
-          marginLeft: screenWidth > 500 ? 60 : 25,
+          marginLeft: screenWidth > 500 ? ms(68) : ms(25),
         },
         tabBarItemStyle: {
-          width: screenWidth > 500 ? 200 : 100,
-          height: screenWidth > 500 ? 60 : 45,
-          paddingHorizontal: 5
+          width: screenWidth > 500 ? ms(180) : ms(100),
+          height: screenWidth > 500 ? ms(60) : ms(45),
+          paddingHorizontal: ms(5)
         },
         tabBarScrollEnabled: true,
         lazy: true,
